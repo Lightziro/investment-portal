d617f3b7e3d8904520932bea9976ce3dff423eca
lightziro <logantuscano@yandex.ru>
2022-01-16 00:18:19 +0300

    page with adaptive sizing

--- nextjs/modules/investment-idea/InvestmentIdeaPage.tsx
+++ nextjs/modules/investment-idea/InvestmentIdeaPage.tsx
@@ -9,6 +9,7 @@ import { ChartStatsAnalytics } from "./components/charts-company-data/ChartStats
 import { CommentsList } from "../../components/smart/comments-list/CommentsList";
 import { createComment } from "../../redux/actions/investmentIdeaActions";
 import { InvestmentIdeaView } from "../../ts/types/redux/store.types";
+import { IdeaDescription } from "./components/idea-description/IdeaDescription";
 interface InvestmentIdeaPage {
     ideaData: InvestmentIdeaView;
 }
@@ -17,54 +18,39 @@ export const InvestmentIdeaPage: React.FC<InvestmentIdeaPage> = ({
 }) => {
     return (
         <Fragment>
-            <Grid container spacing={2}>
-                <Grid item xs={8} container spacing={3}>
-                    <Grid item xs={8}>
-                        <Grid
-                            spacing={2}
-                            container
-                            direction="column"
-                            justifyContent="flex-start"
-                        >
-                            <Grid item>
-                                <IdeaHeader
-                                    companyInfo={ideaData.companyInfo}
-                                />
-                            </Grid>
-                            <Grid item>
-                                <CompanyDescription
-                                    company={ideaData.companyInfo}
-                                />
-                            </Grid>
-                        </Grid>
+            <Grid justifyContent="center" container spacing={2}>
+                <Grid
+                    md={8}
+                    container
+                    item
+                    lg={5}
+                    xl={5}
+                    spacing={2}
+                    direction="column"
+                >
+                    <Grid item>
+                        <IdeaHeader companyInfo={ideaData.companyInfo} />
                     </Grid>
-                    <Grid item xs={4}>
-                        <IdeaAuthor data={ideaData.authorInfo} />
+                    <Grid item>
+                        <CompanyDescription company={ideaData.companyInfo} />
                     </Grid>
                 </Grid>
-                <Grid item xs={4}>
+                <Grid xs={12} sm={4} md={4} item xl={3} lg={3}>
+                    <IdeaAuthor data={ideaData.authorInfo} />
+                </Grid>
+                <Grid xs={12} sm={8} item md={5} xl={4} lg={4}>
                     <ChartStatsEPS epsData={ideaData.epsStats} />
                 </Grid>
-            </Grid>
-            <Divider sx={{ my: 2 }} />
-            <Grid container spacing={2}>
-                <Grid item xs={8} container spacing={3}>
-                    <Grid item xs={7}>
-                        {/*<IdeaDescription description={ideaData.description} />*/}
-                    </Grid>
-                    <Grid item xs={5}>
-                        <IdeaInformation ideaInfo={ideaData.ideaInfo} />
-                    </Grid>
+                <Grid xs={12} item lg={5} sm={7} md={7} xl={5}>
+                    <IdeaDescription description={ideaData.description} />
                 </Grid>
-                <Grid item xs={4}>
+                <Grid item lg={3} md={4} xs={12} sm={5} xl={3}>
+                    <IdeaInformation ideaInfo={ideaData.ideaInfo} />
+                </Grid>
+                <Grid xs={12} md={4} sm={6} item xl={4} lg={4}>
                     <ChartStatsAnalytics stats={ideaData.analyticsStats} />
                 </Grid>
-            </Grid>
-            <Grid container spacing={2}>
-                <Grid item xs={9}>
-                    Test
-                </Grid>
-                <Grid item xs={3}>
+                <Grid sm={6} item xl={3} md={4}>
                     <CommentsList
                         entityId={ideaData.ideaId}
                         entityName="idea"
@@ -73,6 +59,7 @@ export const InvestmentIdeaPage: React.FC<InvestmentIdeaPage> = ({
                     />
                 </Grid>
             </Grid>
+            <Divider sx={{ my: 2 }} />
         </Fragment>
     );
 };
